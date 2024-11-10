d308149599b5ffa055a0f58cfc7828c1fa1bfef9
loftyyyy <lofty08.business@gmail.com>
2024-11-10 14:35:04 +0800

    Updated, fixed, and implemented the layout decoration

--- src/main/frontend/tailwind.config.js
+++ src/main/frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../resources/templates/**/*.{html,js}"],
+  content: ["../resources/templates/**/*.{html,js,svg}"],
   theme: {
     extend: {},
   },
--- src/main/java/org/example/project/cms/TailwindController.java
+++ src/main/java/org/example/project/cms/TailwindController.java
@@ -4,8 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 
 @Controller
