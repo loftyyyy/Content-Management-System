18b1a4955ca8050b82e9d6ef2556f1f854c58077
loftyyyy <lofty08.business@gmail.com>
2024-11-13 23:47:38 +0800

    Fixed and implemented translation for particular languages (so far NL palang)

---
+++ src/main/java/org/example/project/cms/WebMvcConfiguration.java
@@ -0,0 +1,26 @@
+package org.example.project.cms;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
+    @Bean
+    public LocaleChangeInterceptor localeInterceptor() {
+        LocaleChangeInterceptor localeInterceptor = new LocaleChangeInterceptor();
+        localeInterceptor.setParamName("lang");
+        return localeInterceptor;
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeInterceptor());
+    }
+
+}
