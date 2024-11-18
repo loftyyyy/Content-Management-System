fab2c4d39fdae3b41e1f3762b26776c0790daf82
loftyyyy <lofty08.business@gmail.com>
2024-11-18 21:00:35 +0800

    userEntityToFinish

--- src/main/java/org/example/project/cms/Model/User.java
+++ src/main/java/org/example/project/cms/Model/User.java
@@ -1,9 +1,21 @@
 package org.example.project.cms.Model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-//@Entity
+@Entity
 public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+    private String firstName;
+
 
 
 
