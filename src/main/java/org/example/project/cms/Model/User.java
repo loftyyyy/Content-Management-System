a6c0872d6eebea7defeb42d0114b321003ba1d05
loftyyyy <lofty08.business@gmail.com>
2024-11-19 10:07:21 +0800

    added new attributes in the user entity

--- src/main/java/org/example/project/cms/Model/User.java
+++ src/main/java/org/example/project/cms/Model/User.java
@@ -13,16 +13,83 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-
     private String firstName;
     private String lastName;
     private String email;
 
     private String phoneNumber;
 
+    private String gender;
+
+    private String DOB;
 
 
+    public User(Long id, String firstName, String lastName, String email, String phoneNumber, String gender, String DOB) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;
+        this.DOB = DOB;
+    }
 
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
 }
 
 // PAGE 123
